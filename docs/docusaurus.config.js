5513bdf74aaa6e7b569a85a842169eaf00ebb60f
ohmyarthur <ohmyarthurr@gmail.com>
2026-01-19 19:13:29 +0700

    docs : fix project name lowercase to match github pages url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://ohmyarthur.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Kurimod/',
+  baseUrl: '/kurimod/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ohmyarthur', // Usually your GitHub org/user name.
-  projectName: 'Kurimod', // Usually your repo name.
+  projectName: 'kurimod', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
